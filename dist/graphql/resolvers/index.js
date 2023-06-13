8f5d2ccc1173d9ca2ce60c74ca87800e38cd3fcf
Konstantin Nikolaev <you@example.com>
2023-06-12 20:41:54 -0700

    changed package/lock

---
+++ dist/graphql/resolvers/index.js
@@ -0,0 +1,6 @@
+import merge from "lodash.merge";
+import userResolvers from "./user.js";
+import conversationResolvers from "./conversation.js";
+import messageResolvers from "./message.js";
+const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers);
+export default resolvers;
---
+++ dist/graphql/typeDefs/index.js
@@ -0,0 +1,5 @@
+import userTypeDefs from "./user.js";
+import conversationTypeDefs from "./conversation.js";
+import messageTypeDefs from "./message.js";
+const typeDefs = [userTypeDefs, conversationTypeDefs, messageTypeDefs];
+export default typeDefs;
