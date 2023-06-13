8be4cf02efa9c3839c6452da273b62519f0b75ac
Konstantin Nikolaev <you@example.com>
2023-06-12 21:41:10 -0700

    fixed apollo gen

--- dist/graphql/typeDefs/index.js
+++
@@ -1,5 +0,0 @@
-import userTypeDefs from "./user.js";
-import conversationTypeDefs from "./conversation.js";
-import messageTypeDefs from "./message.js";
-const typeDefs = [userTypeDefs, conversationTypeDefs, messageTypeDefs];
-export default typeDefs;
